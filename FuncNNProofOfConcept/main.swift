3aca28702892497c9ab2584803c8f7aea67ac759
Ruslan Shaydulin <ruslan@l2sh.ru>
2016-03-22 16:38:46 +0300

    Initial Commit

---
+++ FuncNNProofOfConcept/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  FuncNNProofOfConcept
+//
+//  Created by Руслан on 22.03.16.
+//  Copyright © 2016 MIPT. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
